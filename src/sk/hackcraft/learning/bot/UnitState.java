893906133836c6f9fd110baa7a695baa20c72e2c
Martin Olešnaník <martinolesnanik@gmail.com>
2015-05-17 15:57:53 +0200

    Work

--- src/sk/hackcraft/learning/bot/UnitState.java
+++ src/sk/hackcraft/learning/bot/UnitState.java
@@ -1,14 +1,35 @@
 package sk.hackcraft.learning.bot;
 
 import sk.hackcraft.bwu.Game;
+import sk.hackcraft.bwu.selection.IntegerComparisonSelector;
+import sk.hackcraft.bwu.selection.UnitSelector;
+import sk.hackcraft.bwu.selection.UnitSelector.BooleanSelector;
 import sk.hackcraft.learning.State;
 import jnibwapi.Unit;
 
-public abstract class UnitState implements State {
-
-	public abstract boolean isUnitInIt(Unit unit);
+public class UnitState implements State {
+	
+	private BooleanSelector selector;
+	
+	public UnitState(BooleanSelector selector) {
+		this.selector = selector;
+	}
+	
+	public boolean isUnitInIt(Unit unit) {
+		return selector.isTrueFor(unit);
+	}
 	
 	public double getValue(Game game, Unit unit) {
-		return 0; // TODO
+		double value = 0;
+		
+		for(Unit myUnit : game.getMyUnits()) {
+			value += myUnit.getHitPoints();
+		}
+		
+		for(Unit enemyUnit : game.getEnemyUnits()) {
+			value -= enemyUnit.getHitPoints();
+		}
+		
+		return value;
 	}
 }
